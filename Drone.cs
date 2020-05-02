9337321904018226a93f5641c885803762f3d1aa
Bruno Noatto Iserhardt <33487330+brunonoatto@users.noreply.github.com>
2020-05-02 16:50:22 -0300

    Add files via upload

---
+++ Drone.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using Algorithm.Logic.Types;
+using Algorithm.Logic.Command;
+using System.Collections.Generic;
+
+namespace Algorithm.Logic
+{
+    /// <summary>
+    /// Classe que contém a regra de negócio de como os comandos são utilizados para movimentar o drone
+    /// </summary>
+    public class Drone
+    {
+        private int LocationX {get; set;}
+        private int LocationY { get; set; }
+        private readonly string InvalidInput = "(999, 999)";
+
+        private List<DroneCommand> ListCommands;
+
+        public Drone()
+        {
+            LocationX = 0;
+            LocationY = 0;
+        }
+        
+        /// <summary>
+        /// Recebe o input, monta os comandos, seta o plano cartesiano e o formata para retorno
+        /// </summary>
+        public string Move(string input)
+        {
+            ListCommands = CommandsBuilder.Create(input);
+
+            if (ListCommands == null)
+                return InvalidInput;
+
+            SetLocations();
+
+            return GetLocation();
+        }
+
+        /// <summary>
+        /// Retorna o plano cartesiano formatado
+        /// </summary>
+        private string GetLocation()
+        {
+            return $"({LocationX}, {LocationY})";
+        }
+        
+        /// <summary>
+        /// Seta o plano cartesiano
+        /// </summary>
+        private void SetLocations()
+        {
+            LocationX = GetXItems().Sum(o => o.GetValue());
+            LocationY = GetYItems().Sum(o => o.GetValue());
+        }
+        
+        private List<DroneCommand> GetXItems()
+        {
+            return ListCommands
+                .Where(o => o.CommandType == CommandType.O || o.CommandType == CommandType.L)
+                .ToList();
+        }
+        
+        private List<DroneCommand> GetYItems()
+        {
+            return ListCommands
+                    .Where(o => o.CommandType == CommandType.S || o.CommandType == CommandType.N)
+                    .ToList();
+        }
+    }
+}
---
+++ Types/CommandType.cs
@@ -0,0 +1,14 @@
+﻿namespace Algorithm.Logic.Types
+{
+    /// <summary>
+    /// Enum que representa os tipos de comandos que o drone suporta
+    /// </summary>
+    public enum CommandType
+    {
+        None,
+        N,
+        S,
+        L,
+        O,
+    }
+}
