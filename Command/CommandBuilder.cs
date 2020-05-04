03c4c82a03425a6436d5675daa3bb6e6faa5fbd9
Bruno Noatto Iserhardt <33487330+brunonoatto@users.noreply.github.com>
2020-05-04 14:44:23 -0300

    new files

---
+++ Command/CommandBuilder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using Algorithm.Logic.Types;
+using System.Collections.Generic;
+
+namespace Algorithm.Logic.Command
+{
+    /// <summary>
+    /// Classe que guarda a regra de negócio de como monta os comandos que irão ser realizados pela classe Drone.
+    /// </summary>
+    public static class CommandsBuilder
+    {
+        public static List<DroneCommand> Create(string input)
+        {
+            if (!InputIsValid(input))
+                return null;
+
+            input= input.ToUpper().Trim();
+
+            List<DroneCommand> result = new List<DroneCommand>();
+            char cancelCommand = 'X';
+            char command;
+            for (int i = 0; i < input.Length; i++)
+            {
+                command = input[i];
+                if (CommandIsValid(command))
+                {
+                    if (command == cancelCommand)
+                    {
+                        if (result.Any())
+                        {
+                            result.Remove(result.Last());
+                        }
+                        else
+                        {
+                            return null; //começou com X ou excedeu a quantidade de comandos removidos
+                        }
+                    }
+                    else
+                    {
+                        int value = GetNextValue(input, ref i);
+                        result.Add(
+                            DroneCommand.Instance((CommandType)Enum.Parse(typeof(CommandType), command.ToString()),
+                                                  value));
+                    }
+                }
+                else
+                {
+                    return null; //commando inválido ou pode ter um expaço no meio
+                }
+            }
+
+            if (!result.All(o => o.IsValid()))
+                return null; //existe algum comando com valor inválido ou não tem tipo
+
+            return result;
+        }
+
+        /// <summary>
+        /// Recebe o índice do comando e verifica se tem algum valor específico para ele no input
+        /// </summary>
+        private static int GetNextValue(string input, ref int i)
+        {
+            int value = 1;
+            if (i < input.Length - 1 && Char.IsDigit(input[i + 1]))
+            {
+                string commandValue = "";
+                int count = 1;
+                while (i + count < input.Length && Char.IsDigit(input[i + count]))
+                {
+                    commandValue = string.Concat(commandValue, input[i + count]);
+                    count++;
+                }
+                i += count - 1;
+                int.TryParse(commandValue, out value);
+            }
+            return value;
+        }
+
+        private static bool CommandIsValid(char command)
+        {
+            char[] validCommands = new char[] { 'N', 'S', 'L', 'O', 'X' };
+            return validCommands.Contains(command);
+        }
+        
+        private static bool InputIsValid(string input)
+        {
+            return !string.IsNullOrWhiteSpace(input);
+        }
+    }
+}
---
+++ Command/DroneCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Algorithm.Logic.Types;
+
+namespace Algorithm.Logic.Command
+{
+    /// <summary>
+    /// Classe que armazena os dados do comando e retorna o valor que deve ser calculado para o plano cartesiano
+    /// </summary>
+    public class DroneCommand
+    {
+        public readonly CommandType CommandType;
+        public readonly int Value;
+
+        public static DroneCommand Instance(CommandType commandType, int value) => new DroneCommand(commandType, value);
+
+        private DroneCommand(CommandType commandType, int value)
+        {
+            CommandType = commandType;
+            Value = value;
+        }
+
+        public int GetValue()
+        {
+            int factor = CommandType == CommandType.S || CommandType == CommandType.O ? -1 : 1;
+            return Value * factor;
+        }
+
+        public bool IsValid()
+        {
+            return Value < Int32.MaxValue && 
+                CommandType != CommandType.None;
+        }
+    }
+}
